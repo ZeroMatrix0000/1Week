f1cdfe2545b7e326101ec186df301c57095b6785
tetsu3009 <it252235@trident.ac.jp>
2026-05-12 19:06:39 +0900

    惑星の動き

---
+++ Assets/WorkSpace/Morizaki/Script/planet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class planet : MonoBehaviour
+{
+    // 回転スピード
+    [SerializeField] private float moveSpeed = 0.0f;
+    // 太陽のゲームオブジェクト
+    GameObject sun;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.sun = GameObject.Find("10_Sun_0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.RotateAround(this.sun.transform.position, Vector3.forward, moveSpeed);
+
+       
+    }
+
+    
+}
